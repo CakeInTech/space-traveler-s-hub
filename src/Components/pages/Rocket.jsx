b418b1da2819766a40bc42f1e65797ed7dc77605
RichardChileya <richardchileyawk@gmail.com>
2022-11-15 20:21:38 +0200

    - initilize useEffect Add dispatch function for rockets

--- src/Components/pages/Rocket.jsx
+++ src/Components/pages/Rocket.jsx
@@ -1,36 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../../Redux/Rockets/Rockets';
 import RocketItem from '../RocketItem';
 
 function Rocket() {
-  const rockets = [
-    {
-      id: 0,
-      name: 'name-1',
-      image: 'img-1',
-      description: 'details-1',
-    },
-    {
-      id: 2,
-      name: 'name-2',
-      image: 'img-2',
-      description: 'details-2',
-    },
-    {
-      id: 3,
-      name: 'name-3',
-      image: 'img-3',
-      description: 'details-3',
-    },
-  ];
+  const dispatch = useDispatch();
+  const rocketSelector = useSelector((state) => state.rockets);
+  console.log(rocketSelector);
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
   return (
     <main>
       <h2>Rocket&lsquo;s page</h2>
       <div>
-        {
-          rockets.map((rocket) => (
-            <RocketItem key={rocket.id} rocket={rocket} />
-          ))
-        }
+        <RocketItem />
       </div>
     </main>
   );
