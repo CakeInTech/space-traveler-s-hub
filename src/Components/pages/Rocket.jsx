324b9841bcda9d882ab7f228d3b8c0c5451cbb43
RichardChileya <richardchileyawk@gmail.com>
2022-11-17 12:53:07 +0200

    Import data from rockets API

--- src/Components/pages/Rocket.jsx
+++ src/Components/pages/Rocket.jsx
@@ -1,19 +1,24 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchRockets } from '../../Redux/Rockets/Rockets';
-import RocketItem from '../RocketItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets, reserveRocket } from '../../Redux/Rockets/Rockets';
+import RocketItem from '../RocketsComponents/RocketItem';
 
 function Rocket() {
   const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const handleReserveRocket = (id) => {
+    dispatch(reserveRocket(id));
+  };
+
   useEffect(() => {
     dispatch(fetchRockets());
   }, [dispatch]);
 
   return (
     <main>
-      <h2>Rocket&lsquo;s page</h2>
+      <hr />
       <div>
-        <RocketItem />
+        <RocketItem rockets={rockets} handleReserveRocket={handleReserveRocket} />
       </div>
     </main>
   );
